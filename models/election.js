79442af62a4bee1b838cb8acc75b093f892529ee
Surendar S <surs@akamai.com>
2021-10-29 19:31:07 +0530

    Updating elections.js

--- models/election.js
+++ models/election.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose'), Schema = mongoose.Schema;
 var electionSchema = new Schema({
     election_id: { type: String, required: true , index: { unique: true }},
     election_name: { type: String, required: true },
-    candidates: { type: [{ name: { type: String, required: true } }] , required: true }
+    candidates: { type: [{ name: { type: String}}]}
 });
 
 module.exports = mongoose.model('electionlist', electionSchema)
